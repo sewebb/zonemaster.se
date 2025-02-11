c49d06f77908b45e7aec599057a964cab82184bb
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-02-11 15:40:05 +0100

    remove import

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,7 +1,6 @@
 /**
  * Main entry point for the javascript bundle.
  */
-import '@internetstiftelsen/styleguide/dist/components';
 import './tab-highlighting/tab-highlighting';
 
 console.log('🎉 Zonemaster is up and running');
