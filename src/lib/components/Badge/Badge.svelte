1088cc74fbaf4f0c5daae82d757cd00c94ad958d
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-06-30 10:09:34 +0200

    merge main repository

--- src/lib/components/Badge/Badge.svelte
+++ src/lib/components/Badge/Badge.svelte
@@ -3,22 +3,15 @@
     import type { ResultLevel } from '@/lib/client.ts';
 
     type Props = {
-<<<<<<< HEAD
         level?: ResultLevel | 'default';
         children?: Snippet;
         round?: boolean;
         border?: boolean;
         size?: 'small' | 'medium';
-=======
-        level: ResultLevel
-        children?: Snippet;
-        round?: boolean;
->>>>>>> 6ff2543 (add new code base)
     };
 
     let {
         children,
-<<<<<<< HEAD
         level = 'default',
         round = false,
         size = 'medium',
@@ -26,12 +19,5 @@
     }: Props = $props();
 </script>
 <span class={['zm-badge', `zm-bg-${level.toLowerCase()}`, `zm-badge--${size}`, round ? 'zm-badge--round' : false, border ? 'zm-badge--border' : false]}>
-=======
-        level,
-        round = false
-    }: Props = $props();
-</script>
-<span class={['zm-badge', `zm-bg-${level.toLowerCase()}`, round ? 'zm-badge--round' : false]}>
->>>>>>> 6ff2543 (add new code base)
   {@render children?.()}
 </span>
