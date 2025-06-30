0275321dedf7e53148b013fa771e15aeef8d89cb
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-06-30 10:09:34 +0200

    merge main repository

--- src/lib/components/DomainTest/ResultModule.svelte
+++ src/lib/components/DomainTest/ResultModule.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-<<<<<<< HEAD
     import type {ResultDataResult, ResultLevel} from "@/lib/client.ts";
     import Stack from "@/lib/components/Stack/Stack.svelte";
     import niceName from "@/lib/niceName.ts";
@@ -7,18 +6,6 @@
     import ResultGroup from "@/lib/components/DomainTest/ResultGroup.svelte";
     import {expandedModules, toggleModule} from "@/lib/components/DomainTest/store.svelte.ts";
     import { resultIcon } from '@/lib/resultIcon.ts';
-=======
-  import type {ResultDataResult, ResultLevel} from "@/lib/client.ts";
-  import Stack from "@/lib/components/Stack/Stack.svelte";
-  import niceName from "@/lib/niceName.ts";
-  import Badge from "@/lib/components/Badge/Badge.svelte";
-  import ResultGroup from "@/lib/components/DomainTest/ResultGroup.svelte";
-  import {expandedModules, toggleModule} from "@/lib/components/DomainTest/store.svelte.ts";
-<<<<<<< HEAD
-  import { resultIcon } from '@/lib/resultIcon.ts';
-=======
->>>>>>> 6ff2543 (add new code base)
->>>>>>> 4a16f44 (add new code base)
 
     type Props = {
         module: string;
@@ -39,7 +26,6 @@
             <i class="bi bi-caret-{open ? 'up' : 'down'}-fill"></i>
             <span>{niceName(module)}</span>
 
-<<<<<<< HEAD
             {#each summary as { level, count }, i}
                 <Badge level={level} round>
                     <i class="bi bi-{resultIcon(level)}"></i>
@@ -57,27 +43,4 @@
             </Stack>
         </div>
     {/if}
-=======
-      {#each summary as { level, count }, i}
-<<<<<<< HEAD
-        <Badge level={level} round>
-            <i class="bi bi-{resultIcon(level)}"></i>
-            {count}
-        </Badge>
-=======
-        <Badge level={level} round>{count}</Badge>
->>>>>>> 6ff2543 (add new code base)
-      {/each}
-    </button>
-  </h3>
-  {#if open}
-  <div class="zm-result__module__data" id={`zmModule-${module}-content`}>
-    <Stack vertical gap="s">
-      {#each Object.entries(grouped) as [testcase, results]}
-        <ResultGroup testcase={testcase} results={results} descriptions={descriptions} />
-      {/each}
-    </Stack>
-  </div>
-  {/if}
->>>>>>> 4a16f44 (add new code base)
 </section>
