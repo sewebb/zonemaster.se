ee144ad06dc1fbc4a74c2f68f1ad92aeae2363ad
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-06-30 10:09:34 +0200

    merge main repository

--- src/lib/components/DomainTest/ResultModule.svelte
+++ src/lib/components/DomainTest/ResultModule.svelte
@@ -1,45 +1,46 @@
 <script lang="ts">
-  import type {ResultDataResult, ResultLevel} from "@/lib/client.ts";
-  import Stack from "@/lib/components/Stack/Stack.svelte";
-  import niceName from "@/lib/niceName.ts";
-  import Badge from "@/lib/components/Badge/Badge.svelte";
-  import ResultGroup from "@/lib/components/DomainTest/ResultGroup.svelte";
-  import {expandedModules, toggleModule} from "@/lib/components/DomainTest/store.svelte.ts";
-<<<<<<< HEAD
-  import { resultIcon } from '@/lib/resultIcon.ts';
-=======
->>>>>>> 6ff2543 (add new code base)
+    import type {ResultDataResult, ResultLevel} from "@/lib/client.ts";
+    import Stack from "@/lib/components/Stack/Stack.svelte";
+    import niceName from "@/lib/niceName.ts";
+    import Badge from "@/lib/components/Badge/Badge.svelte";
+    import ResultGroup from "@/lib/components/DomainTest/ResultGroup.svelte";
+    import {expandedModules, toggleModule} from "@/lib/components/DomainTest/store.svelte.ts";
+    import { resultIcon } from '@/lib/resultIcon.ts';
 
     type Props = {
-        data: Module;
-        summary: ModuleBadge[];
+        module: string;
+        results: ResultDataResult[];
         descriptions: Record<string, string>;
     };
 
-    const { data, descriptions, summary }: Props = $props();
-    const module = data.name;
+    const { module, results, descriptions }: Props = $props();
+    const summary = Object.entries(
+        Object.groupBy(results, ({ level }) => level as ResultLevel)
+    ).map(([level, results]) => ({ level: level as ResultLevel, count: results.length }));
+    const grouped: Partial<Record<string, ResultDataResult[]>> = Object.groupBy(results, ({ testcase }) => testcase);
     let open: boolean = $derived(expandedModules[module] || false);
 </script>
+<section class="zm-result__module" id="zmModule-{module}">
+    <h3 class="zm-result__module__title">
+        <button onclick={() => toggleModule(module)}>
+            <i class="bi bi-caret-{open ? 'up' : 'down'}-fill"></i>
+            <span>{niceName(module)}</span>
 
-      {#each summary as { level, count }, i}
-<<<<<<< HEAD
-        <Badge level={level} round>
-            <i class="bi bi-{resultIcon(level)}"></i>
-            {count}
-        </Badge>
-=======
-        <Badge level={level} round>{count}</Badge>
->>>>>>> 6ff2543 (add new code base)
-      {/each}
-    </button>
-  </h3>
-  {#if open}
-  <div class="zm-result__module__data" id={`zmModule-${module}-content`}>
-    <Stack vertical gap="s">
-      {#each Object.entries(grouped) as [testcase, results]}
-        <ResultGroup testcase={testcase} results={results} descriptions={descriptions} />
-      {/each}
-    </Stack>
-  </div>
-  {/if}
+            {#each summary as { level, count }, i}
+                <Badge level={level} round>
+                    <i class="bi bi-{resultIcon(level)}"></i>
+                    {count}
+                </Badge>
+            {/each}
+        </button>
+    </h3>
+    {#if open}
+        <div class="zm-result__module__data" id={`zmModule-${module}-content`}>
+            <Stack vertical gap="s">
+                {#each Object.entries(grouped) as [testcase, results]}
+                    <ResultGroup testcase={testcase} results={results} descriptions={descriptions} />
+                {/each}
+            </Stack>
+        </div>
+    {/if}
 </section>
