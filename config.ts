4ddd1b9b639fc4ea0af9f9bbb04d0b4936b817ff
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-10-29 14:00:23 +0100

    re-add composer, deploy and theme files

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ import type { Config } from '@/types.ts';
 import packageJson from './package.json';
 
 const config: Config = {
-    defaultLanguage: 'en',
+    defaultLanguage: 'sv',
     enabledLanguages: ['da', 'en', 'es', 'fi', 'fr', 'nb', 'sv', 'sl'],
     baseUrl: import.meta.env.PUBLIC_BASE_URL || '/',
     apiBaseUrl: import.meta.env.PUBLIC_API_URL || '/api',
